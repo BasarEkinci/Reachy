29e7452f193f74c983f29596ebf180e69f239c59
basarekinci <basarekinci02@outlook.com>
2025-10-06 16:44:57 +0300

    Camera Follower Added

---
+++ Reachy/Assets/_GameFolders/Scripts/Functionaries/CameraFollower.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace _GameFolders.Scripts.Functionaries
+{
+    public class CameraFollower : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        [SerializeField] private Vector3 offset;
+        [SerializeField] private float smoothSpeed = 0.125f;
+        
+        private void LateUpdate()
+        {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+        }
+    }
+}
